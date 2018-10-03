4f452596f63021ec8a51dee843312b3f341ca439
Dejan Vasic <dejanvasic24@gmail.com>
2018-10-03 22:31:04 +1000

    Getting started with the navbar for the moment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Buttom from '@material-ui/core/Button';
-import Button from '@material-ui/core/Button';
+import Navbar from './components/Navbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello world</h1>
-        <Button variant="contained" color="primary">Hello again</Button>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <Navbar />
+      </React.Fragment>
     );
   }
 }
